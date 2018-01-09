978fdd82783cd072759a5e62bc0add3802c77af3
Simon Rogers <webdev@centralblue.co.uk>
2018-01-09 20:20:11 +0000

    Additional tests

--- tests/Services/GeoLocationTest.php
+++ tests/Services/GeoLocationTest.php
@@ -4,6 +4,8 @@ namespace Midnite81\GeoLocation\Services\Tests;
 
 use Midnite81\GeoLocation\Services\GeoLocation;
 use Midnite81\GeoLocation\Services\IpLocation;
+use Mockery;
+use Mockery\Mock;
 use PHPUnit\Framework\TestCase;
 
 class GeoLocationTest extends TestCase
@@ -13,6 +15,11 @@ class GeoLocationTest extends TestCase
      */
     protected $geolocation;
 
+    /**
+     * @var GeoLocation
+     */
+    protected $mock;
+
 
     /**
      * @before
@@ -21,6 +28,9 @@ class GeoLocationTest extends TestCase
     {
         $this->geolocation = new GeoLocation();
 
+        $this->mock = Mockery::mock('Midnite81\GeoLocation\Services\GeoLocation[requestData]');
+        $this->mock->shouldAllowMockingProtectedMethods();
+        $this->mock->shouldReceive('requestData')->once()->andReturn($this->results());
     }
     
     /** 
@@ -40,15 +50,63 @@ class GeoLocationTest extends TestCase
         $location = $this->geolocation->get('127.0.0.1', 'address');
     }
 
-//    /**
-//     * @test
-//     */
-//    public function it_accepts_city_as_a_precision()
-//    {
-//
-//        $location = $this->geolocation->get('127.0.0.1', 'city');
-//
-//        $this->assertInstanceOf(IpLocation::class, $location);
-//    }
+    /**
+     * @test
+     */
+    public function it_accepts_city_as_a_precision()
+    {
+
+        $location = $this->mock->get('127.0.0.1', 'city');
+
+        $this->assertInstanceOf(IpLocation::class, $location);
+    }
+
+    /**
+     * @test
+     */
+    public function it_accepts_country_as_a_precision()
+    {
+        $location = $this->mock->get('127.0.0.1', 'country');
+
+        $this->assertInstanceOf(IpLocation::class, $location);
+    }
+
+    /**
+     * @test
+     */
+    public function get_city_returns_data()
+    {
+        $location = $this->mock->getCity('127.0.0.1', 'country');
+
+        $this->assertInstanceOf(IpLocation::class, $location);
+    }
+
+    /**
+     * @test
+     */
+    public function get_country_returns_data()
+    {
+        $location = $this->mock->getCountry('127.0.0.1', 'country');
+
+        $this->assertInstanceOf(IpLocation::class, $location);
+    }
+
+    protected function results()
+    {
+        return json_encode([
+            'statusCode' => '200',
+            'statusMessage' => 'Success',
+            'ipAddress' => '127.0.0.1',
+            'countryCode' => 'gb',
+            'countryName' => 'England',
+            'regionName' => 'London',
+            'cityName' => 'Crystal Palace',
+            'zipCode' => 'S1 E22',
+            'latitude' => '50',
+            'longitude' => '50',
+            'timeZone' => '+00',
+            'addressString' => 'Crystal Palace, London, England'
+        ]);
+    }
 
 }
